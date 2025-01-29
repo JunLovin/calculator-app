2e4ce19a9e2d5aa511df544c78812ea2fd19c690
Mathias Rendon <mathiassaid7@gmail.com>
2025-01-29 17:57:35 -0500

    Style: Add h2 tag, border-bottom and hover animation on it

--- src/components/TextSection.jsx
+++ src/components/TextSection.jsx
@@ -3,6 +3,9 @@ import '@styles/TextSection.css'
 function TextSection() {
     return (
         <>
+            <section className="text-container">
+                <h1>Calculator App - By <a href="https://github.com/JunLovin" target="_blank">JunLovin</a></h1>
+            </section>
         </>
     )
 }
