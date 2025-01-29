230360c3afcbbbd960aa9dad0e3ce302b7cd1521
Mathias Rendon <mathiassaid7@gmail.com>
2025-01-29 17:44:13 -0500

    Link Calculator.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import '@styles/App.css'
 import TextSection from './components/TextSection'
+import Calculator from './components/Calculator'
 
 function App() {
   return (
     <>
+    <Calculator/>
     <TextSection/>
     </>
   )
