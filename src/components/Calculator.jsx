b338b78e26a1f46412e10639eb1761c66b89b926
Mathias Rendon <mathiassaid7@gmail.com>
2025-01-29 17:45:03 -0500

    Create Calculator.jsx and export it

---
+++ src/components/Calculator.jsx
@@ -0,0 +1,12 @@
+import '@styles/Calculator.css'
+
+function Calculator() {
+    return (
+        <>
+        <section className="calculator-container-box">
+        </section>
+        </>
+    )
+}
+
+export default Calculator;
